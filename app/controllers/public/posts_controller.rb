f7694441b2fe4411ac7dd5569565dccabb7e8e9d
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-12-02 17:06:22 +0000

    [Add]post/index,show,edit

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,9 +6,11 @@ class Public::PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -19,12 +21,19 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post.id)
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   def search
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,8 +9,8 @@ class Post < ApplicationRecord
 
   def get_image(width,height)
     unless image.attached?
-      file_path = Rails.root.join('app/asset/images/498417.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg',content_type: 'image/jpg')
+      file_path = Rails.root.join('app/assets/images/498417.jpg')
+      image.attach(io: File.open(file_path), filename:'default-image.jpg', content_type:'image/jpg')
     end
     image.variant(resize_to_limit:[width,height]).processed
   end
