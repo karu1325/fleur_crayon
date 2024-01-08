22973287496e871a323264f04d1b899ccfca29f3
karu1325 <143956677+karu1325@users.noreply.github.com>
2024-01-08 13:42:19 +0000

    [Add]search/controller

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,18 +2,21 @@ class Public::SearchesController < ApplicationController
   def search
     @range = params[:range]
     @keyword = params[:keyword]
-    split_keyword = params[:keyword].split(/[[:blank:]]+/)
+    keyword = params[:keyword].split(/[[:blank:]]+/).join
+    reverse_keyword = params[:keyword].split(/[[:blank:]]+/).sort.join
      #キーワード検索（user,post)
     @posts = []
     @users =[]
     if @range == "Post"
-      split_keyword.each do |keyword|
+      #split_keyword.each do |keyword|
         @posts << Post.search(keyword).order('created_at DESC').page(params[:page]).per(10)
-      end
+        @posts << Post.search(reverse_keyword).order('created_at DESC').page(params[:page]).per(10)
+      #end
     else
-      split_keyword.each do |keyword|
+      #split_keyword.each do |keyword|
         @users << User.search(keyword).order('created_at DESC').page(params[:page]).per(10)
-      end
+        @users << User.search(reverse_keyword).order('created_at DESC').page(params[:page]).per(10)
+      #end
     end
     @posts = @posts.flatten.uniq
     #@posts = @posts.sort_by { |post| post.created_at }.reverse
@@ -25,7 +28,7 @@ class Public::SearchesController < ApplicationController
       @tag = Tag.find(params[:tag_id]) #検索されたタグを受け取る
       @posts = @tag.posts.page(params[:page]).per(10)
       @keyword = @tag.tag_name
-    elsif params[:tag_search].present? #selectのタグ検索
+    elsif params[:tag_search].present? && params[:tag_search] != "" #selectのタグ検索
       @tag = Tag.select("tag_name")
       @keyword = params[:tag_search]
       @posts = Tag.find_by(tag_name: @keyword).posts.page(params[:page]).per(10)
