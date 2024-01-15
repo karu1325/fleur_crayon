bcface6163ffe7eca9bb7ad7e93153f0c61b3789
karu1325 <143956677+karu1325@users.noreply.github.com>
2024-01-15 15:21:55 +0000

    [Add]application.html

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Admin.create!(
-  email:'admin@email.com',
-  password:'admin12'
+  email:'admin@sample.com',
+  password:'admin123'
 )
+
+cray = User.find_or_create_by!(email: "cray@sample.com") do |user|
+  user.name = "Cray"
+  user.password = "password1"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+end
+
+Post.find_or_create_by!(name: "ボールペン") do |post|
+  post.company = "uni"
+  post.caption ="可愛い桜色の本体に使いやすい三色が入った、細くて書き込みに便利なボールペンです！"
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  post.tag_name = "ボールペン"
+  post.user = cray
+end
